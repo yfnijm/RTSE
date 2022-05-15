88916a71d240fe145fa0452852da68f853c9d890
yfnijm <yfnijm@gmail.com>
2022-05-16 00:32:27 +0800

    update makefile

---
+++ source/_RTSE.cpp
@@ -0,0 +1,23 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/attr.h>
+#include <pybind11/numpy.h>
+
+#include <bits/stdc++.h>
+#include "Rtree_non_overlapping.hpp"
+
+
+
+PYBIND11_MODULE(_RTSE, m) {
+	m.doc() = "pybind11 plugin";
+
+
+
+
+}
+
+
+
+
+
+
+
---
+++ source/test.py
@@ -0,0 +1,16 @@
+import pytest
+import numpy
+import time
+import _rtse
+
+def valid_res(size, mat_py, mat):
+	return 
+
+
+def time_cost(size):
+	return 
+
+
+
+def test_performance():
+	return 
